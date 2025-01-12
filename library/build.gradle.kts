9efd1ded46e35c664dfc8fb689c529058b76a318
Anders Kielsholm <anders@anigif.dk>
2025-01-12 01:42:52 +0100

    Initial basic setup of the kmp library

---
+++ library/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+}
+
+group = "dk.anigif.kmp"
+version = "0.1.0"
+
+kotlin {
+    jvm()
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+    linuxX64()
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                // multiplatform dependencies
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(libs.kotlin.test)
+            }
+        }
+    }
+}
