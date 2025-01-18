5fe0fc62f3d67bc07ec619994a63fed5feb1aa42
Anders Kielsholm <anders@anigif.dk>
2025-01-18 22:49:14 +0100

    Make it possible to publish the project to GitHub Packages

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,9 +1,14 @@
+import java.net.URI
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
+
+    id("maven-publish")
 }
 
-group = "dk.anigif.kmp"
+group = "dk.anigif"
 version = "0.1.0"
+val baseArtifactId = "anigif-kmp"
 
 kotlin {
     jvm()
@@ -21,6 +26,27 @@ kotlin {
         val commonTest by getting {
             dependencies {
                 implementation(libs.kotlin.test)
+            }
+        }
+    }
+}
+
+publishing {
+    publications {
+        publications.withType<MavenPublication> {
+            if (artifactId.startsWith(project.name, ignoreCase = true)) {
+                artifactId = artifactId.replaceFirst(project.name, baseArtifactId)
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = URI("https://maven.pkg.github.com/anigif/anigif-kmp")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
             }
         }
     }
