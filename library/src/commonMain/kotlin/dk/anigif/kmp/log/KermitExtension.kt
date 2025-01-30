bc2af0077b1195407e029acc470410cb0d1861b3
Anders Kielsholm <anders@anigif.dk>
2025-01-30 21:28:59 +0100

    Suppress inline warning occuring when compiling (and suppress the suppress-warning that comes as IntelliJ doesn't think the inline suppress is needed)

--- library/src/commonMain/kotlin/dk/anigif/kmp/log/KermitExtension.kt
+++ library/src/commonMain/kotlin/dk/anigif/kmp/log/KermitExtension.kt
@@ -15,6 +15,7 @@ object KermitExtension {
      * Note that this is inline to ensure we get the correct place of cause, but this won't give the correct line
      * number. See youtrack.jetbrains.com/issue/KT-8628 for more information
      */
+    @Suppress("NOTHING_TO_INLINE", "KotlinRedundantDiagnosticSuppress")
     inline fun Logger.unexpected(messageString: String, throwable: Throwable? = null, tag: String = this.tag) {
         log(
             severity = Severity.Error,
